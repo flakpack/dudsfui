087ebd0ebd580c0c4cef312f1d5e427029fdab48
flakpack <flakpack29@gmail.com>
2024-11-05 15:07:15 +0100

    siema

--- zadania adventofcode/zadanie.py
+++ zadania adventofcode/zadanie.py
@@ -2,16 +2,16 @@ import re
 
 with open('../input.txt', 'r') as input_str:
 
-lines = input_str.splitlines()
+    lines = input_str.read().splitlines()
 
-for line in lines:
-    samecyfry = re.sub(r'\D', '', input)
+    for line in lines:
+        samecyfry = re.sub(r'\D', '', input)
 
-    if (len(samecyfry)) == 0:
-        print("")
-    elif (len(samecyfry)) == 1:
-        print(samecyfry * 2)
-    else:
-        pierwsza = samecyfry[0]
-        ostatnia = samecyfry[-1]
-        print(pierwsza + ostatnia)
+        if (len(samecyfry)) == 0:
+            print("")
+        elif (len(samecyfry)) == 1:
+            print(samecyfry * 2)
+        else:
+            pierwsza = samecyfry[0]
+            ostatnia = samecyfry[-1]
+            print(pierwsza + ostatnia)
