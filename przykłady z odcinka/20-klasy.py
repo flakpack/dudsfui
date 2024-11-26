7ec30f6b950d52a62d69adc4040f2204091f6c34
flakpack <flakpack29@gmail.com>
2024-11-26 06:39:41 +0100

    siemaff

--- przykłady z odcinka/20-klasy.py
+++ przykłady z odcinka/20-klasy.py
@@ -1,1 +1,34 @@
-wekfhgdsfigb
+# User
+# Name
+# Mail
+# Age
+
+class User:
+    def __init__(self, name, mail, age):
+        self.name = name
+        self.mail = mail
+        self.age = age
+
+    def print_info(self):
+        print(self.name)
+        print(self.mail)
+        print(self.age)
+
+    def is_male(self):
+        return self.name[-1:] != 'a'
+
+
+
+user1 = User('Michal', 'flakpack29@gmail.com ', 19)
+
+user = User('Adam', 'szy67@gmail.com', 27)
+
+
+user1.print_info()
+user.print_info()
+if user.is_male() == True:
+    print("chłop")
+else:
+    print("baba")
+# print(user.name)
+# print(user1.age)
